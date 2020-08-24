f114acf5fd1684f9a90c927833b97c77247ae3d9
zm1ca <zmicier.fied@yandex.ru>
2020-08-24 14:20:11 +0300

    Initial Commit

---
+++ testRequest/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  testRequest
+//
+//  Created by Źmicier Fiedčanka on 24.08.20.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
